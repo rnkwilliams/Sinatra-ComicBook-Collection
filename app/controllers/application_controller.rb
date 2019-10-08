b97f1f223f790944008be312ba76d759ee35a6c6
Ranika Williams <rnkprice07@gmail.com>
2019-10-08 20:04:29 +0000

    add helpers method to application controller delete helpers app folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def current_user(session_hash)
+        @user ||= User.find(session_hash[:user_id])
+    end
+
+    def is_logged_in?(session_hash)
+        !!session_hash[:user_id]
+    end
 end
